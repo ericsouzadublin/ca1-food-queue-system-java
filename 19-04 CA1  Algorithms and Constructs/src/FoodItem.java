65924dba294b21f8ab23fd1ed020e4067ac68d13
Eric de Souza Teixeira <2025515@student.cct.ie>
2026-04-05 20:47:00 +0100

    Project upload
    Upload Java project files (FoodItem, FoodQueue, Main)

---
+++ 19-04 CA1  Algorithms and Constructs/src/FoodItem.java
@@ -0,0 +1,21 @@
+public class FoodItem {
+    String name;
+    double weight;
+    String bestBeforeDate;
+    String timeAdded;
+
+    public FoodItem(String name, double weight, String bestBeforeDate, String timeAdded) {
+        this.name = name;
+        this.weight = weight;
+        this.bestBeforeDate = bestBeforeDate;
+        this.timeAdded = timeAdded;
+    }
+
+    public void displayItem() {
+        System.out.println("Name: " + name);
+        System.out.println("Weight: " + weight);
+        System.out.println("Best Before Date: " + bestBeforeDate);
+        System.out.println("Time Added: " + timeAdded);
+        System.out.println("--------------------------");
+    }
+}
